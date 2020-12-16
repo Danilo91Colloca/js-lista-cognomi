b1dae735075714c058ff6fd7367d13f812c1a518
Danilo <danilo.colloca91@gmail.com>
2020-12-16 16:48:40 +0100

    restituzione posizione cognome utente nella lista/css  per maiuscole

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 //outputLista
 var printLista = document.getElementById('lista-cognomi')
+
 //questa è la lista dei cognomi
 var surnameList = ['rossi', 'bianchi', 'verdi','scotti','rana','amadori'];
 
@@ -13,8 +14,14 @@ surnameList.push(userSurname);
 surnameList.sort()
 
 //print della lista innerHTML
-
 for (var i = 0; i < surnameList.length; i++) {
-  
   printLista.innerHTML +=  '<li>' + surnameList[i] + '</li>';
 }
+
+//get position userSurname dentro surnameList
+var userPosition = surnameList.indexOf(userSurname);
+
+//print della userPosition
+document.getElementById('position').innerText = 'il tuo cognome è nella posizione numero: ' + (userPosition + 1) + ' della lista';
+
+/*  ho inserito +1 dopo la userPosition poichè il conteggio degli elementi di un array parte dal num. zero, ciò comporterebbe che il primo elemento di una lista ci risulta in posizione zero, aggiungendo il +1 ed essendo la userPosition un numero generato dal method indexOf, il problema si risolve poichè il print ci darà (0 + 1) = 1, (1 + 1) = 2 ecc dove il primo numero dell'addizione è il numero della posizione che l'array ritorna al method indexOf e il +1 è il nostro numero che ci serve per restituri la posizione all utente come se contasse a partire da 1*/
