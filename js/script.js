8765722f906e2767947539d5f2f5592fc043c2ea
Danilo <danilo.colloca91@gmail.com>
2020-12-16 16:10:47 +0100

    inserimento sort() / loop "for" / e print della lista

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
-
+//outputLista
+var printLista = document.getElementById('lista-cognomi')
 //questa è la lista dei cognomi
-var surnameList = ['Rossi', 'Bianchi', 'Verdi','Scotti','Rana','Amadori'];
+var surnameList = ['rossi', 'bianchi', 'verdi','scotti','rana','amadori'];
+
 //il prompt ci darà il cognome dell'utente
-var userSurname = prompt('inserisci il tuo cognome:');
+var userSurname = prompt('inserisci il tuo cognome: (utilizzare caratteri in  minuscolo)');
+
 //push del cognome dell'utente nella lista dei cognomi
 surnameList.push(userSurname);
+
+// sorted in alphabetical order
+surnameList.sort()
+
+//print della lista innerHTML
+
+for (var i = 0; i < surnameList.length; i++) {
+  
+  printLista.innerHTML +=  '<li>' + surnameList[i] + '</li>';
+}
