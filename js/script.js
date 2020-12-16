a36e1a78b1f7e692973a7f742bbf8816d0575972
Danilo <danilo.colloca91@gmail.com>
2020-12-16 15:29:26 +0100

    lista cognomi/inserimento cognome utente by prompt/ push cognome utente

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+//questa è la lista dei cognomi
+var surnameList = ['Rossi', 'Bianchi', 'Verdi','Scotti','Rana','Amadori'];
+//il prompt ci darà il cognome dell'utente
+var userSurname = prompt('inserisci il tuo cognome:');
+//push del cognome dell'utente nella lista dei cognomi
+surnameList.push(userSurname);
